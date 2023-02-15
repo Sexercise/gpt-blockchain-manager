e6c94c51a656c47a6d4760650b66254c68539ed9
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-02-15 07:38:05 +0000

    edit title

--- backend/blockchain_gpt/server.js
+++ backend/blockchain_gpt/server.js
@@ -10,4 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-ap
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-A
