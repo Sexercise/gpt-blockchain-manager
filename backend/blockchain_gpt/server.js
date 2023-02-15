e3942a40e62f544cb5597f71acbcad375bab3351
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-02-15 08:06:10 +0000

    refactor things

--- backend/blockchain_gpt/server.js
+++ backend/blockchain_gpt/server.js
@@ -13,4 +13,11 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-A
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
+var router = express.Route
