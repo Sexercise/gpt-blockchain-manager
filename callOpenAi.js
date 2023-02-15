2afb51a49f7266758da2e4d865c37d66f1aaad89
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-02-15 12:34:05 +0000

    Bump glob-parent from 5.1.0 to 5.1.2

--- callOpenAi.js
+++ callOpenAi.js
@@ -2,4 +2,12 @@ const { Configuration, OpenAIApi } = require("openai");
 const fs = require('fs');
 const dotenv = require('dotenv');
 
-doten
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+
+
+
