1dd335e4917255e57502f16c145783bca157800b
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-06-14 12:15:00 +0000

    signal: fix information leak in copy_siginfo_from_user32

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
 import "./App.css";
 import SuperTokens, { SuperTokensWrapper, getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
 import { SessionAuth } from "supertokens-auth-react/recipe/session";
-import { Routes, BrowserRouter as Router, Route } from "react-rou
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import { SuperTokensConfig } from "./config";
+import Tester from
