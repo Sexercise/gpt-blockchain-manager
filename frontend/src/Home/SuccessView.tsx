ffcd517ad35a0f720b0e536c475b7c954427f978
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-08-18 14:38:40 +0000

    Update trunk

--- frontend/src/Home/SuccessView.tsx
+++ frontend/src/Home/SuccessView.tsx
@@ -1,2 +1,8 @@
 import { useNavigate } from "react-router-dom";
-import { signOut } from "supertokens-auth-r
+import { signOut } from "supertokens-auth-react/recipe/session";
+import {
+  BlogsIcon,
+  CelebrateIcon,
+  GuideIcon,
+  SeparatorLine,
+  Sign
