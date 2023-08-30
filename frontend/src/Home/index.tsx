882846c98db9fe40153e13d3700c4981495c87ce
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-08-30 22:14:31 +0000

    feed changelog

--- frontend/src/Home/index.tsx
+++ frontend/src/Home/index.tsx
@@ -1,2 +1,5 @@
 import SuccessView from "./SuccessView";
-import { useSessionContext } from "supertokens-auth-re
+import { useSessionContext } from "supertokens-auth-react/recipe/session";
+import "./Home.css";
+// TODO: This screen needs to be more professional
+export default function
