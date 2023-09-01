fc9562346e66254103a7ee0c221952c03fd65e6e
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-09-01 16:24:23 +0000

    python 3 fix

--- frontend/src/Home/index.tsx
+++ frontend/src/Home/index.tsx
@@ -2,4 +2,9 @@ import SuccessView from "./SuccessView";
 import { useSessionContext } from "supertokens-auth-react/recipe/session";
 import "./Home.css";
 // TODO: This screen needs to be more professional
-export default function
+export default function Home() {
+    const sessionContext = useSessionContext();
+
+    if (sessionContext.loading === true) {
+        return null;
+    
