2c2ce6a8bada43d02e0e901659ef5b94f9c887a9
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-09-06 08:53:26 +0000

    ASoC: msm: Add bounds checking to ADM get params

--- frontend/src/adapters/ChartComponent.tsx
+++ frontend/src/adapters/ChartComponent.tsx
@@ -1,2 +1,5 @@
 // ChartComponent.tsx
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Line } from 'react-chartjs-2';
+import { Chart, CategoryScale, LinearSca
