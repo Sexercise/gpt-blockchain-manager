a7de14255397c61d3c23f0a4a62181e8b209771c
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-09-07 15:00:47 +0000

    Update front.css

--- frontend/src/adapters/ChartComponent.tsx
+++ frontend/src/adapters/ChartComponent.tsx
@@ -4,4 +4,8 @@ import axios from "axios";
 import { Line } from 'react-chartjs-2';
 import { Chart, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js';
 import 'chartjs-plugin-annotation';
-import { ChartDataset } f
+import { ChartDataset } from 'chart.js/auto';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+
+// Re
