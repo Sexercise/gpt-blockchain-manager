c70c9264a0b39655788b4fec5e103188e4e98edc
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-09-07 08:09:43 +0000

    po: update Polish translation

--- frontend/src/adapters/ChartComponent.tsx
+++ frontend/src/adapters/ChartComponent.tsx
@@ -2,4 +2,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Line } from 'react-chartjs-2';
-import { Chart, CategoryScale, LinearSca
+import { Chart, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js';
+import 'chartjs-plugin-annotation';
+import { ChartDataset } f
