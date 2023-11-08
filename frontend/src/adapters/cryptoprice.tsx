235d69d0e4698b90c890ea82e7c6a75265f6ec78
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-11-08 08:15:59 +0000

    various corrections

--- frontend/src/adapters/cryptoprice.tsx
+++ frontend/src/adapters/cryptoprice.tsx
@@ -2,3 +2,5 @@ import React, { useState } from "react";
 import { _getCryptoCurrencyQuote } from "./market";
 
 const CryptoPrice: React.FC = () => {
+  const [price, setPrice] = useState<number | undefined>(undefined);
+  const [volume, setVolume] 
