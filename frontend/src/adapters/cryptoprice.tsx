7e2432bbca3af17555bf0f4854dc05a0320936aa
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-11-11 05:48:56 +0000

    bump version to 1.0.6

--- frontend/src/adapters/cryptoprice.tsx
+++ frontend/src/adapters/cryptoprice.tsx
@@ -4,4 +4,10 @@ import { _getCryptoCurrencyQuote } from "./market";
 const CryptoPrice: React.FC = () => {
   const [price, setPrice] = useState<number | undefined>(undefined);
   const [volume, setVolume] = useState<number | undefined>(undefined);
-  const [marketCap, setMarketCap] = useState<number
+  const [marketCap, setMarketCap] = useState<number | undefined>(undefined);
+
+  const handleClick = async (quote:any) => {
+
+    switch (quote) {
+      case 'price':
+        
