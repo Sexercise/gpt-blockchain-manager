e3e3fa28c6be4cb7095bcf4f02eb726e6aa51214
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-11-22 18:33:31 +0000

    Compilation fix.

--- frontend/src/adapters/solana_fn.tsx
+++ frontend/src/adapters/solana_fn.tsx
@@ -5,4 +5,7 @@ import { Connection, PublicKey, Version } from "@solana/web3.js";
 export const _connectToPhantomWallet = async (): Promise<null | PhantomWalletAdapter> => {
 
   //@ts-ignore
-  const provider = window
+  const provider = window.solana;
+  if (!provider || !provider.isPhantom) {
+    window.open("https://phantom.app/", "_blank");
+    retu
