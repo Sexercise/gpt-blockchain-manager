f897483d0716b8e932f682976725e0be2697f4e6
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-11-22 17:53:38 +0000

    Updated vendor/ZendFramework

--- frontend/src/adapters/solana_fn.tsx
+++ frontend/src/adapters/solana_fn.tsx
@@ -2,4 +2,7 @@
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import { Connection, PublicKey, Version } from "@solana/web3.js";
 
-export const _connectToPhantomWal
+export const _connectToPhantomWallet = async (): Promise<null | PhantomWalletAdapter> => {
+
+  //@ts-ignore
+  const provider = window
