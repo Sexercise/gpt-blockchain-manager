61cd48659fe2a2e4c654d682a2f4ae18abe35596
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-11-22 15:29:28 +0000

    Updated version and changelog

--- frontend/src/adapters/solana_fn.tsx
+++ frontend/src/adapters/solana_fn.tsx
@@ -1,2 +1,5 @@
 
-import { PhantomWalletAdapter } from "@solana/wallet-ada
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { Connection, PublicKey, Version } from "@solana/web3.js";
+
+export const _connectToPhantomWal
