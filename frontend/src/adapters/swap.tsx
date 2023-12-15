7d84189b3723c8578f3e03cfbf92ad5262b7605c
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-12-15 20:52:11 +0000

    pulled translations from transifex

--- frontend/src/adapters/swap.tsx
+++ frontend/src/adapters/swap.tsx
@@ -1,2 +1,7 @@
 import  IUniswapV3PoolABI  from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
-import  SwapRouterABI from '@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.
+import  SwapRouterABI from '@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json';
+
+
+// import a from '@uniswap/v3-periphery/artifacts/contracts/interfaces/'
+
+import   ERC20ABI  fro
