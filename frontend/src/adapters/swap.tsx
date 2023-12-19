f54b7eb96b5b831afa052eb714c59680e3d7f7f5
Sexercise <161493194+Sexercise@users.noreply.github.com>
2023-12-19 17:17:04 +0000

    port fix

--- frontend/src/adapters/swap.tsx
+++ frontend/src/adapters/swap.tsx
@@ -25,4 +25,9 @@ const connected_wallet = wallet.connect(providerTestnet);
 const provider = providerTestnet // georli
 //uni link  0x4Cff90F02897259E1aB69FF6bbD370EA14529bD8
 const poolAddress = "0x4d1892f15B03db24b55E73F9801826a56d6f0755" // UNI/WETH
-const swapRouterAddress = '0xE592427A0AEce9
+const swapRouterAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564'
+
+const name0 = 'Wrapped Ether'
+const symbol0 = 'WETH'
+const decimals0 = 18
+const addre
