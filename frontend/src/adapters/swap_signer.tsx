75c96c29d83d2255b0a900cd973550aafeae6082
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-01-07 22:13:02 +0000

    Rename Project

--- frontend/src/adapters/swap_signer.tsx
+++ frontend/src/adapters/swap_signer.tsx
@@ -1,4 +1,6 @@
 import ChainId, { Token  } from "@uniswap/sdk-core";
 import { JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
 import { BigNumber, Contract } from "ethers";
-import  SwapRouterABI from '@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRou
+import  SwapRouterABI from '@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json';
+
+const UNISWAP_V3_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; // Example router add
