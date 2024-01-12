d239534aab883a43120c85e15fd86fe7e5990491
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-01-12 07:06:53 +0000

    fixing some bugs

--- frontend/src/adapters/swap_signer.tsx
+++ frontend/src/adapters/swap_signer.tsx
@@ -3,4 +3,9 @@ import { JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
 import { BigNumber, Contract } from "ethers";
 import  SwapRouterABI from '@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json';
 
-const UNISWAP_V3_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; // Example router add
+const UNISWAP_V3_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; // Example router address for Ethereum Mainnet
+
+const {ethers} = require ('ethers')
+
+export const swapTokens = async (
+  tokenInAd
