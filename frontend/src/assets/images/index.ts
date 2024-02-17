a546800658b521e6995bd2c9de0740b43dab036f
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-02-17 17:51:21 +0000

    fix: auto update by markdown generator

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -1,4 +1,8 @@
 import SeparatorLine from "./separator-line.svg";
 import ArrowRight from "./arrow-right-icon.svg";
 import SignOutIcon from "./sign-out-icon.svg";
-import Gu
+import GuideIcon from "./guide-icon.svg";
+import BlogsIcon from "./blogs-icon.svg";
+import CelebrateIcon from "./celebrate-icon.svg";
+
+e
