cfffff3f130175a78c8e9c9b67ab969e4afafe05
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-02-19 22:39:07 +0000

    mm: vmscan: fix the page state calculation in too_many_isolated

--- frontend/src/components/Terminal.tsx
+++ frontend/src/components/Terminal.tsx
@@ -388,3 +388,106 @@ const Terminal: React.FC = () => {
 
       //   // const provider = new JsonRpcProvider(providerUrl);
       //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      //   const signer = provider.getSigner();
+      //   const routerContract = new Contract(
+      //     UNISWAP_V3_ROUTER_ADDRESS,
+      //     SwapRouterABI.abi,
+      //     signer
+      //   );
+
+      //   const network = await provider.getNetwork();
+      //   const chainId = network.chainId;
+
+      //   const tokenIn = new Token(chainId, tokenInAddress, 18); // Assuming 18 decimals
+      //   const tokenOut = new Token(chainId, tokenOutAddress, 18); // Assuming 18 decimals
+
+      //   // Construct the path using the token addresses and the pool's fee tier (500, 3000, or 10000)
+      //   const poolFeeTier = 3000; // Example fee tier
+      //   const path = `0x${tokenInAddress.slice(2)}${tokenOutAddress.slice(
+      //     2
+      //   )}${poolFeeTier.toString(16).padStart(6, "0")}`;
+
+      //   const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from now
+      //   const slippageTolerance = 50; // 0.5% slippage tolerance (50 bips)
+
+      //   // Calculate minimum amountOut based on slippage tolerance
+      //   let init_amount =  ethers.utils.parseEther(amountIn)
+      //   alert (init_amount)
+      //   let amount = BigNumber.from(init_amount);
+      //   alert(amount)
+      //   const amountOutMinimum = amount
+      //     .mul(10000 - slippageTolerance)
+      //     .div(10000);
+
+      //   // Swap tokens
+      //   console.log(routerContract);
+
+      //   // Swap tokens
+      //   const swapTx = await routerContract.exactInputSingle(
+      //     {
+      //       tokenIn: tokenInAddress,
+      //       tokenOut: tokenOutAddress,
+      //       fee: poolFeeTier,
+      //       recipient: account,
+      //       deadline: deadline,
+      //       amountIn: amount.toString(),
+      //       amountOutMinimum: amountOutMinimum.toString(),
+      //       sqrtPriceLimitX96: 0, // Set to 0 for no price limit
+      //     },
+      //     {
+      //       gasLimit: ethers.utils.hexlify(1000000),
+      //     }
+      //   );
+      //   const receipt = await swapTx.wait();
+      //   console.log("receipt ", receipt);
+      //   console.log("Swap transaction hash:", swapTx.hash);
+
+      //    tokenIn_balance = await _getBalance(account , tokenInAddress);
+      //    tokenOut_balance = await  _getBalance(account , tokenOutAddress );
+
+      //   console.log("Your " +tokenInName+ " balance after swap is "+ tokenIn_balance)
+      //   console.log("Your " +tokenOutName+ " balance after swap  is "+ tokenOut_balance)
+
+      // } else {
+      const account = await _getPublicKey();
+      const tokenInAddress = await _getTokenAddress(tokenInName.toUpperCase());
+      const tokenOutAddress = await _getTokenAddress(
+        tokenOutName.toUpperCase()
+      );
+
+      let tokenIn_balance = await _getBalance(account, tokenInAddress);
+      let tokenOut_balance = await _getBalance(account, tokenOutAddress);
+
+      console.log(
+        "Your init ",
+        `${tokenInName}`,
+        " : ",
+        `${tokenInAddress}`,
+        " balance is ",
+        `${tokenIn_balance}`
+      );
+      console.log(
+        "Your init ",
+        `${tokenOutName}`,
+        " : ",
+        `${tokenOutAddress}`,
+        " balance is ",
+        `${tokenOut_balance}`
+      );
+
+      const providerUrl =
+        "https://goerli.infura.io/v3/" + process.env.REACT_APP_INFURA;
+
+      // const provider = new JsonRpcProvider(providerUrl);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      const signer = provider.getSigner();
+      const routerContract = new Contract(
+        UNISWAP_V3_ROUTER_ADDRESS,
+        SwapRouterABI.abi,
+        signer
+      );
+
+      const network = await provider.getNetwork();
+      const chainId = network.chainId;
