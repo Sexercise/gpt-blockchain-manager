4788cd7135853224198c6a8f43afb5bb40b5c328
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-02-24 19:37:04 +0000

    Create calc.lua

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,1 +1,4 @@
-import ThirdPartyEmailPassword, { Google, Github, Apple } from "supertokens-auth-react
+import ThirdPartyEmailPassword, { Google, Github, Apple } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+
+export f
