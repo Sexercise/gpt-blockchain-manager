6fdf531101d559b98f018124a010486018d0fbde
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-02-25 13:17:11 +0000

    Beta1

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -3,4 +3,9 @@ import Session from "supertokens-auth-react/recipe/session";
 
 export function getApiDomain() {
     const apiPort = process.env.REACT_APP_API_PORT || 3001;
-    const apiUrl = process.en
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const 
