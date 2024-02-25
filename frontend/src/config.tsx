49691b75ac56e734d13b5c825c73c35344133724
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-02-25 11:24:40 +0000

    Clean up example

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,4 +1,6 @@
 import ThirdPartyEmailPassword, { Google, Github, Apple } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
 import Session from "supertokens-auth-react/recipe/session";
 
-export f
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.en
