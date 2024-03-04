1892a6971daffd7c75ad4ea0ca731f09d96a2ee1
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-03-04 13:29:39 +0000

    DRM - radeon: Don't link train DisplayPort on HPD until we get the dpcd

--- frontend/src/setupProxy.ts
+++ frontend/src/setupProxy.ts
@@ -2,4 +2,11 @@ import {createProxyMiddleware}  from 'http-proxy-middleware';
 const proxy = {
     target: 'http://authentification:3001',
     changeOrigin: false,
-    secu
+    secure: false
+}
+module.exports = function(app:any) {
+  app.use(
+    '/blockchain',
+    createProxyMiddleware(proxy)
+  );
+  
