b38985ed0f3988ffcffb8ef6d894a938f586ddd1
Sexercise <161493194+Sexercise@users.noreply.github.com>
2024-03-06 15:02:51 +0000

    Fix error message

--- frontend/src/tester.tsx
+++ frontend/src/tester.tsx
@@ -2,4 +2,11 @@ import React from 'react';
 import CryptoPrice from './adapters/cryptoprice';
 import Ethereum from './adapters/ethereum';
 import ChartComponentTest from './adapters/ChartComponentTest';
-import Solana from './adapt
+import Solana from './adapters/solana';
+
+
+const Tester: React.FC = () => {
+  return (
+    <div>
+      <h1>Crypto Tester: </h1>
+        <CryptoPrice ></C
